5efa62b77605be59f62c8e351c0d1e96d7b8cc09
Alex <leha.asd95@mail.ru>
2017-04-16 09:51:09 +0600

    dev: add view pager

--- app/src/main/java/com/nividimka/yandextranslater/ui/MainActivity.java
+++ app/src/main/java/com/nividimka/yandextranslater/ui/MainActivity.java
@@ -1,15 +1,41 @@
 package com.nividimka.yandextranslater.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.nividimka.yandextranslater.R;
+import com.nividimka.yandextranslater.ui.adapter.MainViewPagerAdapter;
+import com.nividimka.yandextranslater.ui.fragments.BookmarkFragment;
+import com.nividimka.yandextranslater.ui.fragments.SettingsFragment;
+import com.nividimka.yandextranslater.ui.fragments.TranslateFragment;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements BookmarkFragment.OnFragmentInteractionListener,SettingsFragment.OnFragmentInteractionListener, TranslateFragment.OnFragmentInteractionListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        MainViewPagerAdapter adapter = new MainViewPagerAdapter(MainActivity.this,getSupportFragmentManager());
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+
+        tabLayout.setSelectedTabIndicatorHeight(4);
+
+        //set drawables for each tab
+        tabLayout.getTabAt(0).setIcon(R.drawable.tab1_translate_icon);
+        tabLayout.getTabAt(1).setIcon(R.drawable.tab2_bookmark_icon);
+        tabLayout.getTabAt(2).setIcon(R.drawable.tab3_settings_icon);
+//        tabLayout.getTabAt(3).setIcon(R.mipmap.ic_launcher);
+//        tabLayout.getTabAt(4).setIcon(R.mipmap.ic_launcher_round);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/nividimka/yandextranslater/ui/adapter/MainViewPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.nividimka.yandextranslater.ui.adapter;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.nividimka.yandextranslater.ui.fragments.BookmarkFragment;
+import com.nividimka.yandextranslater.ui.fragments.SettingsFragment;
+import com.nividimka.yandextranslater.ui.fragments.TranslateFragment;
+
+
+public class MainViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    public MainViewPagerAdapter(Activity activity, FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return new TranslateFragment();
+            case 1:
+                return new BookmarkFragment();
+            case 2:
+                return new SettingsFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return " ";
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
